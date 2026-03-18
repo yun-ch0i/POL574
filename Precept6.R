d6536d49e720049d03bb045de40aee3ce25e0376
Yun Choi <choiyun0830@gmail.com>
2026-03-18 17:15:22 -0400

    Upload Precept 6 materials

---
+++ Precept6.R
@@ -0,0 +1,225 @@
+
+## POL 574: Quantitative Analysis IV
+## Date: March 19, 2026
+## Author: Yun Choi
+
+## Lab adapted from: Christian Baehr, Elisa Wirsching, Lucia Motolinia, 
+## Pedro L. Rodriguez, Kevin Munger, Patrick Chester and Leslie Huang.
+
+## Topics:
+## - Supervised Learning Workflow
+## - LASSO/Ridge Regression
+## - ROC Curves
+## - Bradley-Terry
+
+################################################# Precept 6: Supervised Text Analysis III
+
+## package dependencies
+pacman::p_load(dplyr, glmnet, quanteda, ROCR, caret, purrr, tidyr)
+
+## 1.1) Load in news data ------------------------------------------------------
+
+## load news data
+news_data <- readRDS("data/news_data.rds")
+
+## let's work with 2 categories
+news_samp <- news_data %>% 
+  filter(category %in% c("WEIRD NEWS", "GOOD NEWS")) %>% 
+  select(headline, category) %>% 
+  setNames(c("text", "class"))
+
+## get a sense of how the text looks
+dim(news_samp)
+head(news_samp$text[news_samp$class == "WEIRD NEWS"])
+head(news_samp$text[news_samp$class == "GOOD NEWS"])
+
+## some pre-processing (the rest will let dfm do)
+news_samp$text <- gsub(pattern = "'", "", news_samp$text)  # replace apostrophes
+news_samp$class <- ifelse(news_samp$class=="WEIRD NEWS", 0, 1)
+
+## what's the distribution of classes?
+prop.table(table(news_samp$class))
+
+## create document feature matrix
+news_dfm <- tokens(news_samp$text, remove_punct = T) %>% 
+  dfm() %>% 
+  dfm_remove(stopwords("en")) %>% 
+  dfm_wordstem() %>% 
+  convert("matrix")
+dim(news_dfm)
+
+
+## 1.2) Break into training and test sets --------------------------------------
+
+## partial out 2/3 of rows to train with
+
+y <- news_samp$class
+X <- news_dfm
+
+train.indx <- sample(1:nrow(X), floor(2/3 * nrow(X)))
+
+y.train <- y[train.indx]
+X.train <- X[train.indx,]
+
+y.test <- y[-train.indx]
+X.test <- X[-train.indx,]
+
+## 1.3) k-fold CV to determine hyperparameters ---------------------------------
+
+## Ridge and Lasso combine the classical OLS optimization problem (minimize SSR)
+## with an additional penalty for large coefficients. The purpose of cross-validating
+## in this case is to determine HOW MUCH we should penalize for coefficient size. 
+## And then to estimate the coefficients.
+
+## as lambda grows -> more attention to shrinking coefficient size than improving fit
+
+help(glmnet)
+## alpha parameter [0,1] determines mix between L1 and L2 loss. alpha=0 means 
+## all L2 loss, i.e. Ridge
+
+## CV for Ridge model
+cvout.ridge <- cv.glmnet(X.train, y.train, alpha=0) # default to 10-fold CV
+plot(cvout.ridge)
+
+cvout.ridge$lambda.min # best lambda
+cvout.ridge$lambda.1se # simpler lambda
+cvout.ridge$cvm # mean CV error
+cvout.ridge$cvsd # std error
+
+## predict values of test set, given the value of lambda and the 
+pred.ridge <- predict(cvout.ridge, s="lambda.min", newx=X.test) # s= value of lambda; 
+
+## how do we know arguments?
+## class(model) -> ?predict.<class>
+# helpful visualization: https://devopedia.org/bias-variance-trade-off
+
+## raw prediction error
+error <- pred.ridge - y.test
+
+## create df with actual, predicted, error, text
+predictions <- cbind.data.frame(y.test, pred.ridge, error, news_samp$text[-train.indx]) |>
+  setNames(c("actual", "predicted", "error", "text"))
+
+
+
+## 1.4) QUIZ -------------------------------------------------------------------
+
+##### which terms do we think are more "WEIRD" and less "GOOD"? #####
+
+
+## 2.1) IN CLASS ACTIVITY ------------------------------------------------------
+
+## Manually generate a ROC curve with the False Positive rate on the x-axis
+## and True Positive rate on the y. In this case, "Positive" means that news 
+## was predicted as "good".
+
+## HINT: FP Rate = FP / (FP+TN)
+##       TP Rate = TP / (TP+FN)
+
+pred.ridge
+y.test
+
+threshold <- 0.2
+
+fp <- sum((pred.ridge > 0.2) & y.test==0)
+tp <- sum((pred.ridge > 0.2) & y.test==1)
+
+fn <- sum((pred.ridge <= 0.2) & y.test==1)
+tn <- sum((pred.ridge <= 0.2) & y.test==0)
+
+fp.rate <- fp / (fp+tn)
+tp.rate <- tp / (tp+fn)
+
+roc <- function(threshold) {
+  fp <- sum((pred.ridge > threshold) & y.test==0)
+  tp <- sum((pred.ridge > threshold) & y.test==1)
+  
+  fn <- sum((pred.ridge <= threshold) & y.test==1)
+  tn <- sum((pred.ridge <= threshold) & y.test==0)
+  
+  fp.rate <- fp / (fp+tn)
+  tp.rate <- tp / (tp+fn)
+  return(c( fp.rate, tp.rate ))
+}
+
+rates <- seq(0.1, 1, 0.1)
+
+roc.data <- sapply(rates, roc)
+roc.data <- t(roc.data)
+
+plot(roc.data)
+
+## 2.2) ROC Curves -------------------------------------------------------------
+
+## generate a set of cutoffs -- predict GOOD/WEIRD at each cutoff
+pred.cutoff <- prediction(pred.ridge, y.test)
+
+## evaluate TP/FP rate at each cutoff
+pred.tpr <- performance(pred.cutoff, measure="tpr", x.measure="fpr")
+
+## plot results
+plot(pred.tpr, col="red") # plot ROC curve
+lines(c(0,1),c(0,1), col = "black", lty = 4) # baseline
+
+
+## 2.3) AUC values -------------------------------------------------------------
+
+## evaluate AUC across cutoffs
+pred.auc <- performance(pred.cutoff, measure = "auc")
+pred.auc@y.values[[1]] # extract the total area under curve
+
+## Bradley-Terry ----------------------------------------------------------
+data("citations", package = "BradleyTerry2")
+
+View(citations)
+
+citations.sf <- countsToBinomial(citations)
+names(citations.sf)[1:2] <- c("journal1", "journal2")
+
+citeModel <- BTm(cbind(win1, win2), journal1, journal2,
+                 refcat = "Biometrika",
+                 data = citations.sf)
+
+summary(citeModel)
+
+abilities <- as.data.frame(BTabilities(citeModel))
+abilities$id <- rownames(abilities)
+abilities$lower <- abilities$ability - 1.96 * abilities$s.e.
+abilities$upper <- abilities$ability + 1.96 * abilities$s.e.
+
+# sort by ability
+abilities <- abilities[order(abilities$ability), ]
+
+dotchart(abilities$ability,
+         labels = abilities$item,
+         xlim = range(abilities$lower, abilities$upper),
+         pch = 19,
+         main = "Item abilities with 95% CIs")
+
+# add error bars
+for (i in seq_len(nrow(abilities))) {
+  lines(c(abilities$lower[i], abilities$upper[i]), c(i, i))
+}
+
+# An different data structure
+citations_long <- citations.sf %>%
+  mutate(row_id = row_number()) %>%
+  pivot_longer(
+    cols = c(win1, win2),
+    names_to = "win_type",
+    values_to = "n"
+  ) %>%
+  mutate(
+    winner = if_else(win_type == "win1", journal1, journal2),
+    loser = if_else(win_type == "win1", journal2, journal1)
+  ) %>%
+  uncount(n) %>%
+  select(winner, loser)
+
+head(citations_long)
+
+citeModel <- BTm(outcome = 1, winner, loser,
+                 refcat = "Biometrika",
+                 data = citations_long)
+
+summary(citeModel)
