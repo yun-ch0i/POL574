32e3a4bda81d56b5f9cf9907bf02b29ce5e8ea5e
Yun Choi <choiyun0830@gmail.com>
2026-04-20 11:03:33 -0400

    Helpful code for HW3 Q8.2

---
+++ Homeworks/Homework3/HW3_helpful_code.R
@@ -0,0 +1,47 @@
+# This script will be helpful in solving HW3 Q8.2
+
+library(conText)
+library(quanteda)
+
+# Load corpus
+data("data_corpus_inaugural")
+corp <- corpus(data_corpus_inaugural)
+# Load pretrained embeddings
+glove <- readRDS("data/glove.rds")
+
+# Set document variable
+docvars(corp)$party_simple <- ifelse(docvars(corp)$Party == "Democratic", "Dem", "Other")
+
+# Tokenize
+toks <- tokens(
+  corp,
+  remove_punct = TRUE,
+  remove_numbers = TRUE
+) |>
+  tokens_tolower()
+
+# Feature Co-occurrence Matrix (FCM)
+fcm_mat <- fcm(
+  toks,
+  context = "window",
+  window  = 5,
+  count   = "weighted",
+  weights = 1 / (1:5),
+  tri     = FALSE
+)
+
+# Compute corpus-specific transform
+transform_mat <- compute_transform(
+  x = fcm_mat,
+  pre_trained = glove,
+  weighting = "log"
+)
+
+# Feature Embedding Matrix (FEM)
+fem_mat <- fem(
+  x = fcm_mat,
+  pre_trained = glove,
+  transform = TRUE,
+  transform_matrix = transform_mat,
+  verbose = TRUE
+)
